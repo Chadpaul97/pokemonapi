969c666c340ebcb340099d3e061e833e5ccdeb1e
Chadpaul97 <chadpaul97@gmail.com>
2022-01-22 14:06:06 -0600

    Using Axios

--- src/component/FetchPokemon.jsx
+++ src/component/FetchPokemon.jsx
@@ -1,16 +1,19 @@
 import React, {useState} from 'react';
-
+import axios from 'axios';
 export default function FetchPokemon() {
     const [pokemon, setPokemon] = useState([]);
     let count = 1
     const onClickHandler = () => {
-        fetch ("https://pokeapi.co/api/v2/pokemon?limit=807")
-        .then(response => response.json())
-        .then(response => {
-            console.log(response)
-            setPokemon(response.results)
-        })
-        .catch(err => console.log(err))
+        // fetch ("https://pokeapi.co/api/v2/pokemon?limit=807")
+        // .then(response => response.json())
+        // .then(response => {
+        //     console.log(response)
+        //     setPokemon(response.results)
+        // })
+        // .catch(err => console.log(err))
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+        .then(response=>{setPokemon(response.data.results)})
+        .catch(err=>console.log(err))
 
     }
 
@@ -22,9 +25,9 @@ export default function FetchPokemon() {
             <button onClick={onClickHandler}>click me !</button>
             </div>
             <div>
-                {pokemon.map((pokemon,i) =>{
+            {pokemon.map((pokemon,i) =>{
                     return <div key={i}><span> {count++} {pokemon.name}</span></div>
-                })}
+                })} 
             </div>
         </div >
     )
